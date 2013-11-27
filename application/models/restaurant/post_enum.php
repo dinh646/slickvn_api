6af46d5537b49f24ceb75490fdb6941d35e31d5f
xinhhuynh <xinhhuynh@innoria.com>
2013-11-27 15:37:58 +0700

    add const REATE_POINT, NUMBER_ASSESSMENT into file post_enum

--- application/models/restaurant/post_enum.php
+++ application/models/restaurant/post_enum.php
@@ -15,7 +15,6 @@ class Post_enum {
    const ID                     = 'id';
    const ID_USER                = 'id_user';
    const TITLE                  = 'title';
-   
    const AVATAR                 = 'avatar';
    const ADDRESS                = 'address';
    const FAVOURITE_TYPE_LIST    = 'favourite_type_list';
@@ -23,6 +22,8 @@ class Post_enum {
    const CULINARY_STYLE_LIST    = 'culinary_style_list';
    
    const CONTENT                = 'content';
+   const RATE_POINT             = 'rate_point';
+   const NUMBER_ASSESSMENT      = 'number_assessment';
    const NUMBER_VIEW            = 'number_view';
    const NOTE                   = 'note';
    const AUTHORS                = 'authors';
